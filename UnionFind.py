69769205c2546da5585c80c4b001ec351b96fce8
abhiverma866 <abhiverma866@gmail.com>
2017-11-25 21:17:13 +0530

    Update UnionFind.py

--- UnionFind.py
+++ UnionFind.py
@@ -1,8 +1,6 @@
 """UnionFind.py
 
-Union-find data structure. Based on Josiah Carlson's code,
-http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/215912
-with significant additional changes by D. Eppstein.
+Union-find data structure. 
 """
 
 class UnionFind:
